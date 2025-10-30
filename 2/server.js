6bf9f1e65778b3fa6f67159e15e42116abee5895
Sameer <akita.sameer1@gmail.com>
2025-10-30 09:28:18 +0900

    first endpoints

--- 2/server.js
+++ 2/server.js
@@ -2,4 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
+app.get('/', (req, res) => {
+    res.send('<h1>This is a website</h1><input />')
+})
+
+app.get('/dashboard', (req, res) => {
+    console.log('hit the /dashboard endpoint')
+    res.send('hi')
+})
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
