fd9f74a75d554ab73121e9c26c8faa7624e3cf9c
Sameer <akita.sameer1@gmail.com>
2025-10-30 09:39:04 +0900

    api endpoint

--- 2/server.js
+++ 2/server.js
@@ -2,13 +2,21 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
+let data = {
+    name: 'james'
+}
+
 app.get('/', (req, res) => {
-    res.send('<h1>This is a website</h1><input />')
+    res.send('<h1>Home</h1>')
 })
 
 app.get('/dashboard', (req, res) => {
-    console.log('hit the /dashboard endpoint')
-    res.send('hi')
+    res.send('<h1>Dashboard</h1>')
+})
+
+app.get('/api/data', (req, res) => {
+    console.log('This one was for data')
+    res.send(data)
 })
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
