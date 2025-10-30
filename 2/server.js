fc0b7a977babdbbb80416f6975428f5909475ee5
Sameer <akita.sameer1@gmail.com>
2025-10-30 09:52:00 +0900

    post request

--- 2/server.js
+++ 2/server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
-let data = {
-    name: 'james'
-}
+let data = ['james']
+
+// Middleware
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home</h1>')
+    res.send(`
+        <body style="background: pink; color: blue;">
+            <h1>Data: </h1>
+            <p>${JSON.stringify(data)}</p>
+        </body>
+    `)
 })
 
 app.get('/dashboard', (req, res) => {
@@ -17,6 +23,12 @@ app.get('/dashboard', (req, res) => {
 app.get('/api/data', (req, res) => {
     console.log('This one was for data')
     res.send(data)
+})
+
+app.post('/api/data', (req, res) => {
+    const newEntry = req.body
+    data.push(newEntry.name)
+    res.sendStatus(201)
 })
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
