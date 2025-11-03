0222bd2e6fabfedec4e9dafc98466a6e40534ad0
Sameer <akita.sameer1@gmail.com>
2025-11-03 20:34:34 +0900

    serve html file

--- 3/src/server.js
+++ 3/src/server.js
@@ -1,9 +1,23 @@
 import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 1234
 
-console.log('hello world')
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+// Serves the HTML file from the /public category
+// Tells express to serve all files from the public folder as static assets / files.
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`)
