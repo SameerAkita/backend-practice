a8ef16ece7c62ecfb51b7b4599c98edfc2f6d959
Sameer <akita.sameer1@gmail.com>
2025-11-03 20:45:27 +0900

    sqlite db

--- 3/src/server.js
+++ 3/src/server.js
@@ -10,6 +10,8 @@ const __filename = fileURLToPath(import.meta.url)
 // Get the directory name from the file path
 const __dirname = dirname(__filename)
 
+// Middleware
+app.use(express.json())
 // Serves the HTML file from the /public category
 // Tells express to serve all files from the public folder as static assets / files.
 app.use(express.static(path.join(__dirname, '../public')))
