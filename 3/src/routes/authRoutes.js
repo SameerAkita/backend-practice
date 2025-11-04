cd5b44dc4db1bc0503778175da73ac6d3df059a5
Sameer <akita.sameer1@gmail.com>
2025-11-05 05:50:22 +0900

    /auth/login logic

--- 3/src/routes/authRoutes.js
+++ 3/src/routes/authRoutes.js
@@ -29,8 +29,22 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
     const { username, password } = req.body
-    console.log(username, password)
-    res.sendStatus(201)
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser.get(username)
+
+        if (!user) {return res.status(404).send({ message: "User not found" })}
+        
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        if (!passwordIsValid) {return res.status(401).send({ message: "Invalid password" })}
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 })
 
 export default router
