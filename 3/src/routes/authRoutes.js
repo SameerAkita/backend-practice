d0f7add0fcce78785d1d959216ec40bf3c6d1fc2
Sameer <akita.sameer1@gmail.com>
2025-11-05 02:36:27 +0900

    auth/register route logic

--- 3/src/routes/authRoutes.js
+++ 3/src/routes/authRoutes.js
@@ -7,8 +7,24 @@ const router = express.Router()
 
 router.post('/register', (req, res) => {
     const { username, password } = req.body
-    console.log(username, password)
-    res.sendStatus(201)
+    const hashedPassword = bcrypt.hashSync(password, 8)
+    
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+
+        // Add default todo
+        const defaultTodo = `Hello! Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 })
 
 router.post('/login', (req, res) => {
