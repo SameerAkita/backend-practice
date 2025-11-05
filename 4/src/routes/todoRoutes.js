eb6be436bb618611166daa557a2651b156f35273
Sameer <akita.sameer1@gmail.com>
2025-11-06 03:40:34 +0900

    getTodos sqlite to prisma

--- 4/src/routes/todoRoutes.js
+++ 4/src/routes/todoRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import db from '../db.js'
+import prisma from '../prismaClient.js'
 
 const router = express.Router()
 
 // Get all todos for logged-in user
-router.get('/', (req, res) => {
-    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
-    const todos = getTodos.all(req.userId)
+router.get('/', async (req, res) => {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId: req.userId
+        }
+    })
     res.json(todos)
 })
 
