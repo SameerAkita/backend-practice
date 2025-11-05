e344b9bb367301aab8f1c513281f7d0b40b9840b
Sameer <akita.sameer1@gmail.com>
2025-11-05 15:05:01 +0900

    get todos

--- 3/src/routes/todoRoutes.js
+++ 3/src/routes/todoRoutes.js
@@ -3,8 +3,12 @@ import db from '../db.js'
 
 const router = express.Router()
 
-// Get all tokens for logged-in user
-router.get('/', (req, res) => {})
+// Get all todos for logged-in user
+router.get('/', (req, res) => {
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
+})
 
 // Create new todo
 router.post('/', (req, res) => {})
