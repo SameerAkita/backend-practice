96dbf2fa4736c6a5035266212b61baed37d38637
Sameer <akita.sameer1@gmail.com>
2025-11-05 15:52:07 +0900

    createTodo

--- 3/src/routes/todoRoutes.js
+++ 3/src/routes/todoRoutes.js
@@ -11,7 +11,13 @@ router.get('/', (req, res) => {
 })
 
 // Create new todo
-router.post('/', (req, res) => {})
+router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+    const result = insertTodo.run(req.userId, task)
+
+    res.json({ id: result.lastInsertRowid , task, completed: 0})
+})
 
 // Update a todo
 router.put('/:id', (req, res) => {})
